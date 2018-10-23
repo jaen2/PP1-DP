d794d820305d9bb5afdeb6c96c1e7bc06fd6d3ca
jaen2 <21700151@handong.edu>
2018-10-23 21:55:49 +0900

    Task4

--- src/test/java/edu/handong/csee/pp1/dp/CLAMITest.java
+++ src/test/java/edu/handong/csee/pp1/dp/CLAMITest.java
@@ -75,8 +75,7 @@ public class CLAMITest {
 		
 		// TASK5 TODO Use this method to find the best cutoff
 		
-		String bestCutoff = "50";
-		
+		String bestCutoff = "88";
 		String[] args = {"-f","data/Apache.arff","-l","isDefective","-p", "TRUE", "-m", "-c", bestCutoff};
 
 		CLAMI.main(args);
